0b3a9fbd417b2de7880062d9473c418db8869b0d
DemonicBenefactor <demonic@tutanota.de>
2023-12-29 14:52:19 +0200

    modified:   World.tscn 	new file:   scenes/Player.tscn 	modified:   source/Player.gd

--- source/Player.gd
+++ source/Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
-var MAX_SPEED = 100
+const MAX_SPEED = 100
+const ACCELERATION = 550
+const DECELERATION = 600
+
 var velocity = Vector2.ZERO
 var inputVector = Vector2.ZERO
 
@@ -10,10 +13,12 @@ func _ready():
 func _physics_process(delta):
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")	
+	inputVector = inputVector.normalized()
 	
 	if inputVector != Vector2.ZERO:
-		velocity = inputVector
+		velocity = velocity.move_toward(inputVector * MAX_SPEED, ACCELERATION * delta)
+
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
 		
-	move_and_collide(velocity * delta * MAX_SPEED)
+	velocity = move_and_slide(velocity)
